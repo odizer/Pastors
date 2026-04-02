5fd38cd90aef76411a121043a0b725a3df9a600d
odizer <odhiambovictor042@gmail.com>
2026-04-02 15:22:43 +0300

    my first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+pastors = ["Kanyari", "Mbadi", "Too", "Victor"]
+offering_Kanyari = 10
+expenses_Kanyari = 6500
+salary_Kanyari = 1000
+networth_Kanyari = salary_Kanyari - offering_Kanyari - expenses_Kanyari
+if networth_Kanyari > salary_Kanyari:
+    print("Kanyari is Corrupt")
+else:
+      print("Kanyari is not corupt")
---
+++ readme.md
@@ -0,0 +1,1 @@
+#Fake pastors tracking system
